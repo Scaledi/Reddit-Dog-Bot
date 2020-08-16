e25f12ecc4c1c18f52b542e1f9c4510dcc3b650e
Scaledish <ebrobot2000@gmail.com>
2020-08-16 15:09:41 -0400

    added basic login... as well as a message

--- bot.py
+++ bot.py
@@ -2,3 +2,23 @@ import praw # pip install praw (not a default package)
 import random
 import json
 import time
+import config
+import re
+import requests
+from datetime import datetime
+
+# Login
+reddit = praw.Reddit(username = config.username,
+                    password = config.password,
+                    client_id = config.client_id,
+                    client_secret = config.client_secret,
+                    user_agent = "DogB0t By u/bobdarobber")
+print("finished logging in")
+
+if config.test_online == True:
+    testsubmission = reddit.submission(id=config.online_id)
+    print("working...")
+    now = datetime.now()
+    currenttime = now.strftime("%D:%H:%M")
+    testsubmission.reply("booted at " + currenttime)
+    print("finished. staus=Ok")
