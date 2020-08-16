ff8ff54b8339b7412491a455670589e15a5efb67
Scaledish <ebrobot2000@gmail.com>
2020-08-16 15:10:38 -0400

    git ignore login details

---
+++ templateconfig.py
@@ -0,0 +1,9 @@
+username = ""
+password = ""
+client_id = ""
+client_secret = ""
+
+test_online = True # if you want a message to be posted on boot
+online_id = "" # The place to post the message
+
+# Template Config file. rename to config.py
