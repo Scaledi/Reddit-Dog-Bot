550ef2c71645d888c1e9079fb3bd2329b8048b53
Evan Boehs <ebrobot2000@gmail.com>
2020-09-14 16:29:14 -0400

    Stop responding to same valid message twice. #1

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+# region: importing
 import praw # pip install praw (not a default package)
 import re
 import pdb
@@ -8,15 +9,22 @@ import time
 import config
 import requests
 from datetime import datetime
+# endregion
+# region: variables
+footer = """
 
-# Login
+---
+
+This is a stupid bot made by a stupid human who makes stupid mistakes. Report issues and send hate [HERE](https://github.com/Scaledi/Send-Dog-Pics-Reddit)
+"""
+# endregion
+# region: login/debug
 reddit = praw.Reddit(username = config.username,
                     password = config.password,
                     client_id = config.client_id,
                     client_secret = config.client_secret,
                     user_agent = "DogB0t By u/bobdarobber")
 print("finished logging in")
-
 
 # just debbugging stuff. you will want to keep this off, unless.
 if config.test_online == True:
@@ -26,11 +34,13 @@ if config.test_online == True:
     currenttime = now.strftime("%D:%H:%M")
     testsubmission.reply("booted at " + currenttime)
     print("finished. staus=Ok")
+# endregion
 
-subreddit = reddit.subreddit("bobdarobber")
+subreddit = reddit.subreddit("bobdarobber") # The subs to respond in
 
 for comment in subreddit.stream.comments():
-    print(comment.body)
-    if re.search("had (a)?(my)? dog", comment.body, re.IGNORECASE):
-            comment.reply("Hi")
+    if comment.saved == False:
+        if re.search("had a dog", comment.body, re.IGNORECASE): # Had a dog
+            comment.save()
+            comment.reply("Damn... really sorry to hear that." + footer)
             print("done")
