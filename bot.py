58c2534ce4fc2e2012e86e0768444da58517ed62
Evan Boehs <ebrobot2000@gmail.com>
2020-09-17 11:30:46 -0400

    added def settings

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@ import praw # pip install praw (not a default package)
 import re
 import pdb
 import os
-
 import json
 import time
 import config
@@ -38,43 +37,50 @@ if config.test_online == True:
     currenttime = now.strftime("%D:%H:%M")
     testsubmission.reply("booted at " + currenttime)
     print("finished. staus=Ok")
-
-if config.show_feed == True:
-    for comment in subreddit.stream.comments():
+# endregion
+# region: extra config
+def listcomments(): 
+    if config.show_feed == True:
         print(comment.body)
         print("- " + str(comment.author))
         print(comment.subreddit)
         print("---")
+
+def ifrated():
+    if config.is_rated == True:
+        time.sleep(600)
+    else:
+        time.sleep(1)
 # endregion
 # region: responding
 for comment in subreddit.stream.comments():
     if comment.saved == False:
-        if re.search("(had a dog|my dog died)", comment.body, re.IGNORECASE): # Had a dog/my dog died
+        listcomments()
+        if re.search("((my|our) (dog|doggo|pet) (died|got killed)|(we|I) put (our|my) (dog|doggo|pet) to sleep)", comment.body, re.IGNORECASE): # Had a dog/my dog died
            comment.save()
            comment.reply("sorry for your loss :(. have any pictures? I would love to see your dog." + footer)
            print("done t0")
-           time.sleep(600)
+           ifrated()
         elif re.search("(has|got|fed|feed|walk|walked) (a|my) dog", comment.body, re.IGNORECASE): # Has a dog
             comment.save()
             comment.reply("Cool! Send dog pics!" + footer)
             print("done t1")
-            time.sleep(600)
+            ifrated()
         elif re.search("want a dog", comment.body, re.IGNORECASE): # want a dog
             comment.save()
             comment.reply("What type of dog do you want?... and... find any cute pictures of that breed?" + footer)
             print("done t2")
-            time.sleep(600)
+            ifrated()
         elif re.search("(ate|killed) (my|a) dog", comment.body, re.IGNORECASE): # ate a dog
             comment.save()
             comment.reply("Cursed." + footer)
             print("done a damn monster")
-            time.sleep(600)
+            ifrated()
 
 # Code Documentation
 # comment.save is the method used to avoid responding to the same comments if the bot reboots.
 # re.search("regex string", comment.body, re.IGNORECASE) = regex.search("regex string", incommentbody, ignoring case)
 
 # endregion
-
 # region: except
 # endregion
--- templateconfig.py
+++ templateconfig.py
@@ -4,7 +4,8 @@ client_id = ""
 client_secret = ""
 
 test_online = True # if you want a message to be posted on boot
-online_id = "" # The place to post the message
-show_feed = False
+online_id = "" # The id of the place to post the message that the bot is online
+show_feed = True # show a feed of submissions.
+is_rated = True # is your bot crashing due to being rate limited?
 
 # Template Config file. rename to config.py
